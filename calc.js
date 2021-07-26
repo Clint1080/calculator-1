54d613f5d528e6bf1ef11e28d471c4df9fc645f7
Clint Edwards <cedwards1080@gmail.com>
2021-07-26 16:11:57 -0600

    Extra credit

--- calc.js
+++ calc.js
@@ -7,7 +7,9 @@ const reader = readline.createInterface({
 });
 
 
-
+console.log('You can use +, -, /, *. For ex. + 2 4 will = 8');
+console.log('sqrd is to square a number and sqrt is for square root');
+console.log('** raised your first number to the power of the second number')
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
 	
@@ -15,6 +17,33 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	// console.log(`${num1}` + `${num2}`);
+	if (mathSymbol === "+"){
+        console.log(num1 + num2);
+	}
+	else if (mathSymbol === "-"){
+        console.log(num1 - num2);
+	}
+	else if (mathSymbol === "/"){
+        console.log(num1 / num2);
+	} 
+	else if (mathSymbol === "*"){
+        console.log(num1 * num2);
+	} 
+	else if (mathSymbol === "sqrt"){
+        console.log(`the square root of ${num1} is ${Math.sqrt(num1)}`);
+	}
+	else if (mathSymbol === "sqrd"){
+        console.log(`${num1} squared is ${num1 * num1}`);
+	} 
+	else if (mathSymbol === "**"){
+        console.log(`${num1} to the power of ${num2} is ${num1 ** num2}`);
+	} 
+	else if (mathSymbol === "%"){
+		var remainder = num1 / num2;
+		var fraction = remainder % 1;
+        console.log(fraction);
+	} 
 
 	// This line closes the connection to the command line interface.
 	reader.close()
